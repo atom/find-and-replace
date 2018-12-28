2e1a680a8bd30ae7bd3cb5c6ba5595d77f92eff4
Winston Liu <50Wliu@users.noreply.github.com>
2018-12-27 22:54:39 -0500

    SPACES

--- spec/find-spec.js
+++ spec/find-spec.js
@@ -3,28 +3,28 @@ const {it, fit, ffit, beforeEach, afterEach} = require('./async-spec-helpers') /
 const BufferSearch = require('../lib/buffer-search')
 
 describe('Find', () => {
-	describe('updating the find model', () => {
-		beforeEach(async () => {
-			const activationPromise = atom.packages.activatePackage('find-and-replace')
-			atom.commands.dispatch(atom.views.getView(atom.workspace), 'find-and-replace:show')
-			await activationPromise
+  describe('updating the find model', () => {
+    beforeEach(async () => {
+      const activationPromise = atom.packages.activatePackage('find-and-replace')
+      atom.commands.dispatch(atom.views.getView(atom.workspace), 'find-and-replace:show')
+      await activationPromise
 
-			spyOn(BufferSearch.prototype, 'setEditor')
-		})
+      spyOn(BufferSearch.prototype, 'setEditor')
+    })
 
-		it("sets the find model's editor whenever an editor is focused", async () => {
-			let editor = await atom.workspace.open()
-			expect(BufferSearch.prototype.setEditor).toHaveBeenCalledWith(editor)
+    it("sets the find model's editor whenever an editor is focused", async () => {
+      let editor = await atom.workspace.open()
+      expect(BufferSearch.prototype.setEditor).toHaveBeenCalledWith(editor)
 
-			editor = await atom.workspace.open('sample.js')
-			expect(BufferSearch.prototype.setEditor).toHaveBeenCalledWith(editor)
-		})
+      editor = await atom.workspace.open('sample.js')
+      expect(BufferSearch.prototype.setEditor).toHaveBeenCalledWith(editor)
+    })
 
-		it("sets the find model's editor to null if a non-editor is focused", async () => {
-			spyOn(atom.workspace, 'isTextEditor').andReturn(false)
+    it("sets the find model's editor to null if a non-editor is focused", async () => {
+      spyOn(atom.workspace, 'isTextEditor').andReturn(false)
 
-			await atom.workspace.open()
-			expect(BufferSearch.prototype.setEditor).toHaveBeenCalledWith(null)
-		})
-	})
+      await atom.workspace.open()
+      expect(BufferSearch.prototype.setEditor).toHaveBeenCalledWith(null)
+    })
+  })
 })
